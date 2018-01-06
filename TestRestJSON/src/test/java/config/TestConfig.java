699b44c56ecc32037a5cca29f8a3b286e42a372b
nmschorr <nancyschorr@yahoo.com>
2018-01-06 14:40:12 -0800

    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

--- TestRestJSON/src/test/java/config/TestConfig.java
+++ TestRestJSON/src/test/java/config/TestConfig.java
@@ -8,40 +8,51 @@ import  io.restassured.specification.ResponseSpecification;
 import org.junit.BeforeClass;
 
 public class TestConfig {
-    public static RequestSpecBuilder typicodebuilder;
-    public static RequestSpecification typicodeSpec;
- 
-	public static RequestSpecification arspec;
-    public static ResponseSpecification responseSpec;
+	public static RequestSpecBuilder typicodebuilder;
+	public static RequestSpecification typicodeSpec;
+	public static RequestSpecification blobSpec;
+	
+	public static ResponseSpecification responseSpec;
+	public static ResponseSpecBuilder responseSpecBuilder;
 
-    @BeforeClass
+	@BeforeClass
 	public static void SetUp() {
-    	RestAssured.proxy("localhost", 8888);	
+		RestAssured.proxy("localhost", 8888);	
+													//https://jsonblob.com/53998288-f328-11e7-8877-3d11de9ec1d3
+												// other :  "/d14860ba-f2a0-11e7-8877-13767a00f47a";
+		String blobBase        = "https://jsonblob.com";
+		String blobBasePath = "53998288-f328-11e7-8877-3d11de9ec1d3";
+		blobSpec = new RequestSpecBuilder().
+				setBaseUri(blobBase).
+				setBasePath(blobBasePath).
+				addHeader("ContentType", "application/json").
+				//    addHeader("ContentType", "application/json; charset=utf-8").
+				addHeader("Accept", "application/json").
+				build();
+	 
+		//RestAssured.requestSpecification = blobSpec;
+	
+	//TestConfig.SetUpAlbums();
+	TestConfig.SetUpResponse();
+}
+	  // ----------------------------------------------------- Response: 
+	static void SetUpAlbums() {
 
-    	typicodebuilder = new RequestSpecBuilder();
-    	typicodebuilder.setBaseUri("http://jsonplaceholder.typicode.com");     
-    	typicodebuilder.setBasePath("/albums");      
-    	typicodebuilder.setPort(80);
-    	typicodebuilder.addHeader("ContentType", "application/json");
-        typicodeSpec = typicodebuilder.build();
-    RestAssured.requestSpecification = typicodeSpec;
-      
-       String mybase        = "https://jsonblob.com";
-       String mybasepath = "/d14860ba-f2a0-11e7-8877-13767a00f47a";
-		arspec = new RequestSpecBuilder().
-				setBaseUri(mybase).
-			    setBasePath(mybasepath).
-			    //addHeader("ContentType", "application/json").
-			    							//    addHeader("ContentType", "application/json; charset=utf-8").
-			    addHeader("Accept", "application/json").
+		typicodebuilder = new RequestSpecBuilder();
+		typicodebuilder.setBaseUri("http://jsonplaceholder.typicode.com");     
+		typicodebuilder.setBasePath("/albums");      
+		typicodebuilder.setPort(80);
+		typicodebuilder.addHeader("ContentType", "application/json");
+		typicodeSpec = typicodebuilder.build();
+		//RestAssured.requestSpecification = typicodeSpec;   // sets this for all tests
+}
+
+		static void SetUpResponse() {
+
+			responseSpec = new ResponseSpecBuilder().
+			    expectStatusCode(200).
 				build();
 
-	//RestAssured.requestSpecification = arspec;
-			
-    		responseSpec = new ResponseSpecBuilder() 
-				.expectStatusCode(200)
-				.build();
-		
-		 RestAssured.responseSpecification = responseSpec;    
- }
+		RestAssured.responseSpecification = responseSpec;    // sets this for all tests
+	}
 }
